b149ef81bd936289c5b2bbb8be70a6ddd6402137
Sudhir Sundrani <ssundrani@gmail.com>
2023-07-09 21:56:26 +0530

    modified:   data_gen_load/content_gen_function.py 	modified:   requirements.txt

--- data_gen_load/content_gen_function.py
+++ data_gen_load/content_gen_function.py
@@ -38,43 +38,43 @@ class get_event_metadata(BaseModel):
 ai("First iPhone announcement", output_schema=get_event_metadata)
 
 
-# Initialize the chat model
-chat_model = ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo-16k', openai_api_key=openai_key)
+# # Initialize the chat model
+# chat_model = ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo-16k', openai_api_key=openai_key)
 
-def generate_article(topic):
-    # The schema I want out
-    response_schemas = [
-        ResponseSchema(name="Content", description="article about prompt in around 1000 tokens. Add logical markdowns if missing in Content"),
-        ResponseSchema(name="Title", description="article headline to generate maximum clicks"),
-        ResponseSchema(name="Category", description="The category of the article."),
-        ResponseSchema(name="Products", description="items I can sell to person reading this article"),
-        ResponseSchema(name="Summary", description="Artistic summary of the article in 2 sentences.Use simple language that start with the main subject, such as 'Portrait of woman','unicorn with tiger stripes', 'giant robot standing' etc, followed by descriptive terms like 'beautiful lighting','ultra-realistic', 'graceful', 'detailed face' etc")
+# def generate_article(topic):
+#     # The schema I want out
+#     response_schemas = [
+#         ResponseSchema(name="Content", description="article about prompt in around 1000 tokens. Add logical markdowns if missing in Content"),
+#         ResponseSchema(name="Title", description="article headline to generate maximum clicks"),
+#         ResponseSchema(name="Category", description="The category of the article."),
+#         ResponseSchema(name="Products", description="items I can sell to person reading this article"),
+#         ResponseSchema(name="Summary", description="Artistic summary of the article in 2 sentences.Use simple language that start with the main subject, such as 'Portrait of woman','unicorn with tiger stripes', 'giant robot standing' etc, followed by descriptive terms like 'beautiful lighting','ultra-realistic', 'graceful', 'detailed face' etc")
         
-    ]
+#     ]
 
-    # The parser that will look for the LLM output in my schema and return it back to me
-    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+#     # The parser that will look for the LLM output in my schema and return it back to me
+#     output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
-    # The format instructions that LangChain makes. Let's look at them
-    format_instructions = output_parser.get_format_instructions()
+#     # The format instructions that LangChain makes. Let's look at them
+#     format_instructions = output_parser.get_format_instructions()
 
-    # The prompt template that brings it all together
-    # Note: This is a different prompt template than before because we are using a Chat Model
-    prompt = ChatPromptTemplate(
-    messages=[
-        HumanMessagePromptTemplate.from_template("For Indian audiences write an article on: {topic}\n \
-                                                    {format_instructions}\n{user_prompt}")
-    ],
-    input_variables=["topic", "user_prompt"],
-    partial_variables={"format_instructions": format_instructions}
-    )
+#     # The prompt template that brings it all together
+#     # Note: This is a different prompt template than before because we are using a Chat Model
+#     prompt = ChatPromptTemplate(
+#     messages=[
+#         HumanMessagePromptTemplate.from_template("For Indian audiences write an article on: {topic}\n \
+#                                                     {format_instructions}\n{user_prompt}")
+#     ],
+#     input_variables=["topic", "user_prompt"],
+#     partial_variables={"format_instructions": format_instructions}
+#     )
 
-    article_query = prompt.format_prompt(topic=topic, user_prompt=topic)
+#     article_query = prompt.format_prompt(topic=topic, user_prompt=topic)
 
-    article_output = chat_model(article_query.to_messages())
-    output = output_parser.parse(article_output.content)
+#     article_output = chat_model(article_query.to_messages())
+#     output = output_parser.parse(article_output.content)
     
-    return output
+#     return output
 
 # Loading inputs
 filepath = './inputdummy.json'
